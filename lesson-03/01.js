d2d4f7e64142ab12b7c7f37b9cb4111a5205edf6
Vad <ichetowwwkin@mail.ru>
2024-03-29 23:04:12 +0300

    lesson3 1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,10 @@
 // code
+
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(it=> it === "черный" || it === "красный" || it === "желтый").join('-');
+    // ФИЛЬТРУЕМ.ПРЕВРАЩАЕМ В СТРОКУ('-')
+}
+
+console.log(createColorString());
